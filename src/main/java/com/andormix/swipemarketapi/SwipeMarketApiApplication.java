8c2ce839874297d07b41357483c815ea701e1c3c
Eric Torrontera Ruiz <eric@aNDORRA.AD>
2026-09-21 01:30:33 +0200

    feat(setup): initial project setup and files

---
+++ src/main/java/com/andormix/swipemarketapi/SwipeMarketApiApplication.java
@@ -0,0 +1,13 @@
+package com.andormix.swipemarketapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SwipeMarketApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SwipeMarketApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/andormix/swipemarketapi/SwipeMarketApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.andormix.swipemarketapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SwipeMarketApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
