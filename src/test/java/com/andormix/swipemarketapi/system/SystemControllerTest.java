892802e4b1cab3510c3c336caf706e773f9b3df5
Eric Torrontera Ruiz <eric@aNDORRA.AD>
2026-09-21 19:25:50 +0200

    feat(test): Updated auth controller tests

--- src/test/java/com/andormix/swipemarketapi/system/SystemControllerTest.java
+++ src/test/java/com/andormix/swipemarketapi/system/SystemControllerTest.java
@@ -1,24 +1,25 @@
 package com.andormix.swipemarketapi.system;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
 import static org.hamcrest.Matchers.is;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@SpringBootTest
-@ActiveProfiles("test")
-@AutoConfigureMockMvc(addFilters = false)
 class SystemControllerTest {
 
-    @Autowired
     private MockMvc mockMvc;
+
+    @BeforeEach
+    void setUp() {
+        mockMvc = MockMvcBuilders
+                .standaloneSetup(new SystemController())
+                .build();
+    }
 
     @Test
     void shouldReturnApplicationStatus() throws Exception {
